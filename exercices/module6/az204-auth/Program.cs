af4d39956b617e13ba730ca8f4d9dabd191fb9dd
Trung Thai <trthai@gmail.com>
2023-11-08 21:28:34 -0500

    initial draft learning dotnet and cosmos for fun

---
+++ exercices/module6/az204-auth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "605756fe-261f-4d2e-bf75-2f0cf70caee4";
+        private const string _tenantId = "a087e33a-f318-4bb4-8bb1-dd86c151a52f";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build(); 
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
